3fee088978609c1aeaa803dd23fd5f2992308339
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-13 22:19:08 -0600

    Add new modules for employees, advances, absences, payments, users, and vacations. Update base path in router.php.

--- routes/router.php
+++ routes/router.php
@@ -6,24 +6,48 @@ require_once __DIR__ . '/session.php';
 $router = new AltoRouter();
 
 // Configurar la base del path si tu proyecto está en un subdirectorio
-$router->setBasePath('/app');
+$router->setBasePath('/rrhh-dismafer'); // Ajusta este path según tu estructura
 
 // Definir rutas
+$router->map('GET', '/', function() {
+    require __DIR__ . '/../modules/home/index.php';
+});
+
 $router->map('GET', '/empleados', function() {
     require __DIR__ . '/../modules/empleados/index.php';
 });
 
-// $router->map('GET', '/nombre-de-modulo/submodulo', function() {
-//     require __DIR__ . '/../modules/nombre-de-modulo/submodulo.php'; // Ajusta la ruta según sea necesario
-// });
+$router->map('GET', '/adelantos', function() {
+    require __DIR__ . '/../modules/adelantos/index.php';
+});
 
-$router->map('GET', '/auth/login', function() {
+$router->map('GET', '/ausencias', function() {
+    require __DIR__ . '/../modules/ausencias/index.php';
+});
+
+$router->map('GET', '/pagos', function() {
+    require __DIR__ . '/../modules/pagos/index.php';
+});
+
+$router->map('GET', '/usuarios', function() {
+    require __DIR__ . '/../modules/usuarios/index.php';
+});
+
+$router->map('GET', '/vacaciones', function() {
+    require __DIR__ . '/../modules/vacaciones/index.php';
+});
+
+$router->map('GET', '/login', function() {
     require __DIR__ . '/../modules/auth/login.php';
 });
 
-$router->map('POST', '/auth/login', function() {
-    // Aquí podrías incluir la lógica de procesamiento del login o redirigir a otro archivo que lo maneje
+$router->map('GET', '/register', function() {
+    require __DIR__ . '/../modules/auth/register.php';
 });
+
+// $router->map('POST', '/auth/login', function() {
+//     require __DIR__ . '/../modules/auth/backend/procesar_login.php';
+// });
 
 // Aquí puedes seguir agregando rutas según necesites
 
