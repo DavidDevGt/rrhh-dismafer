60eeac2d0824114dd1ad507fe2ae2417b154a060
DavidDevGt <josuedavidvl18@gmail.com>
2023-12-14 22:17:38 -0600

    Add functions to handle employee addition, editing, and deletion

--- modules/empleados/ajax.php
+++ modules/empleados/ajax.php
@@ -25,10 +25,50 @@ switch ($accion) {
 }
 
 function agregarEmpleado()
-{}
+{
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $puesto = $_POST['puesto'];
+    $telefono = $_POST['telefono'];
+    $estado_civil = $_POST['estado_civil'];
+    $correo = $_POST['correo'];
+    $fecha_nacimiento = $_POST['fecha_nacimiento'];
+    $fecha_inicio = $_POST['fecha_inicio'];
+    $direccion = $_POST['direccion'];
+    $dpi = $_POST['dpi'];
+    $activo = isset($_POST['activo']) ? 1 : 0;
+
+    $query = "INSERT INTO empleados (nombres, apellidos, puesto, telefono, estado_civil, correo, fecha_nacimiento, fecha_inicio, direccion, dpi, activo) VALUES (" . dbEscape($nombres) . ", " . dbEscape($apellidos) . ", " . dbEscape($puesto) . ", " . dbEscape($telefono) . ", " . dbEscape($estado_civil) . ", " . dbEscape($correo) . ", " . dbEscape($fecha_nacimiento) . ", " . dbEscape($fecha_inicio) . ", " . dbEscape($direccion) . ", " . dbEscape($dpi) . ", " . dbEscape($activo) . ")";
+    if (dbQuery($query)) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'error' => 'Error al agregar empleado']);
+    }
+}
 
 function editarEmpleado()
-{}
+{
+    $id_empleado = $_POST['id_empleado'];
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $puesto = $_POST['puesto'];
+    $telefono = $_POST['telefono'];
+    $estado_civil = $_POST['estado_civil'];
+    $correo = $_POST['correo'];
+    $fecha_nacimiento = $_POST['fecha_nacimiento'];
+    $fecha_inicio = $_POST['fecha_inicio'];
+    $direccion = $_POST['direccion'];
+    $dpi = $_POST['dpi'];
+    $activo = isset($_POST['activo']) ? 1 : 0;
+
+    $query = "UPDATE empleados SET nombres = '" . dbEscape($nombres) . "', apellidos = '" . dbEscape($apellidos) . "', puesto = '" . dbEscape($puesto) . "', telefono = '" . dbEscape($telefono) . "', estado_civil = '" . dbEscape($estado_civil) . "', correo = '" . dbEscape($correo) . "', fecha_nacimiento = '" . dbEscape($fecha_nacimiento) . "', fecha_inicio = '" . dbEscape($fecha_inicio) . "', direccion = '" . dbEscape($direccion) . "', dpi = '" . dbEscape($dpi) . "', activo = " . dbEscape($activo) . " WHERE id_empleado = " . dbEscape($id_empleado);
+    if (dbQuery($query)) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'error' => 'Error al editar empleado']);
+    }
+
+}
 
 function eliminarEmpleado()
 {
